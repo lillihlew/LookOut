acb4e7da76c90b06af658525280bea3fc8a3db7f
lillihlew <lillihlew@gmail.com>
2025-03-20 20:28:14 -0500

    got app up to where we need to edit index

---
+++ StickerSmash/app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
