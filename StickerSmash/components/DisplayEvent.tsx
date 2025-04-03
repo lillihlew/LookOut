06d79e4da71403f5a357f560cd2be949325b2277
lillihlew <lillihlew@gmail.com>
2025-04-03 11:43:00 -0500

    added scrolling to event prototype

--- StickerSmash/components/DisplayEvent.tsx
+++ StickerSmash/components/DisplayEvent.tsx
@@ -1,22 +1,30 @@
 import SharedStyles from "@/app/styles"
 import ImageViewer from "./ImageViewer"
 import { useState } from "react"
-import { View, Text } from "react-native";
-
+import { View, Text, ScrollView } from "react-native";
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 const DisplayEvent = ({selectedImage, selectedDate, selectedTitle, selectedDescription, selectedPrivacyOn}:any) =>{
+    const [date, time] = selectedDate.toLocaleString().split(',');
     return(
-        <View style = {SharedStyles.container}>
-            <ImageViewer imgSource={selectedImage}/>
-            <Text style = {SharedStyles.infoText}>{selectedTitle}</Text>
-            <Text style = {SharedStyles.textWhite}>{selectedDescription}</Text>
-            <Text style = {SharedStyles.textWhite}>{selectedDate.toLocaleString()}</Text>
-            {selectedPrivacyOn ? 
-                <Text style = {SharedStyles.textWhite}>Private Event</Text> 
-                : 
-                <Text style = {SharedStyles.textWhite}>Public Event</Text>
-            }
-        </View>
+        <SafeAreaProvider>
+            <ScrollView>
+                <SafeAreaView style = {SharedStyles.container}>
+                    {/* <View style = {SharedStyles.imageContainer}> */}
+                        <ImageViewer imgSource={selectedImage}/>
+                        <Text style = {SharedStyles.infoText}>{selectedTitle}</Text>
+                    {/* </View> */}
+                    <Text style = {SharedStyles.textWhite}>{date}</Text>
+                    <Text style = {SharedStyles.textWhite}>{time}</Text>
+                    {selectedPrivacyOn ? 
+                        <Text style = {SharedStyles.textWhite}>Private Event</Text> 
+                        : 
+                        <Text style = {SharedStyles.textWhite}>Public Event</Text>
+                    }
+                    <Text style = {SharedStyles.textWhite}>{selectedDescription}</Text>
+                </SafeAreaView>
+            </ScrollView>
+        </SafeAreaProvider>
     )
     
 }
